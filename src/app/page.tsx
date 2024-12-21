51b8611d67ef59cbe01fe1f4edd2ff1c0490ad99
Yaseer123 <yaseerarafat230@gmail.com>
2024-12-21 15:55:43 +0600

    gg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import PercentageCalculator from "@/components/calculators/PercentageCalculator/PercentageCalculator";
 import PercentageCalculatorDescription from "@/components/calculators/PercentageCalculator/PercentageCalculatorDescription";
 
@@ -14,9 +15,27 @@ export const metadata = {
         "percentage change",
     ],
 };
+
 const Home = () => {
     return (
         <div>
+            <Head>
+                {/* Google tag (gtag.js) */}
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-EVGW2WPZ0Z"
+                ></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-EVGW2WPZ0Z');
+                        `,
+                    }}
+                />
+            </Head>
             <PercentageCalculator />
             <PercentageCalculatorDescription />
         </div>
